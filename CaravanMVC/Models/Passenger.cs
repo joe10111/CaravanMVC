03022832454281949b41dd16dc913f7b459234e7
joe10111 <centeno.joe@gmail.com>
2023-08-21 12:28:34 -0700

    Added Id

--- CaravanMVC/Models/Passenger.cs
+++ CaravanMVC/Models/Passenger.cs
@@ -2,6 +2,7 @@
 {
     public class Passenger
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
         public string Destination { get; set; }
