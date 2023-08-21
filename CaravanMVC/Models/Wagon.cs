2b8476f38823ba7983fd0140562117e968ee4428
joe10111 <centeno.joe@gmail.com>
2023-08-21 12:19:15 -0700

    Added Models  Wagon and Passenger

---
+++ CaravanMVC/Models/Wagon.cs
@@ -0,0 +1,19 @@
+﻿namespace CaravanMVC.Models
+{
+    public class Wagon
+    {
+        public string Name { get; set; }
+        public int NumWheels { get; set; }
+        public bool Covered { get; set; }
+        public Passenger Passengers { get; set; }
+
+        public Wagon() { }
+        public Wagon(string name, int numWheels, bool covered, Passenger passengers)
+        {
+            Name = name;
+            NumWheels = numWheels;
+            Covered = covered;
+            Passengers = passengers;
+        }
+    }
+}
